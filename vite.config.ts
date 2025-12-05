2cfdea9996b1b77cee38213a3085aeb5fb328241
ZHOU\zhouhao <1170745135@qq.com>
2025-12-06 02:28:25 +0800

    配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/BD2merchant/',
   plugins: [
     vue(),
     vueDevTools(),
