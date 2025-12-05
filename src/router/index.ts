ee7058e0ce3aa55c6c25c37fa80ac6582293b4f7
ZHOU\zhouhao <1170745135@qq.com>
2025-12-06 02:37:56 +0800

    路由模式切换

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CommodityView from '../views/commodity/CommodityView.vue'
 import SenceView from '../views/SenceView.vue'
@@ -6,7 +6,7 @@ import charactarView from '../views/charactar/charactarView.vue'
 import shaftView from '../views/Shaft/shaftView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
